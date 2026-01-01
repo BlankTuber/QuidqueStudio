43f7d2a753d6c4fdf2f73a7bde23964b6077134d
Mumeisho <blank@quidque.no>
2026-01-01 19:54:18 +0100

    Starting over with frontend design

--- templates/blog/show.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?= htmlspecialchars($post['title']) ?> - <?= $config['site']['name'] ?></title>
-</head>
-<body>
-    <h1><?= htmlspecialchars($post['title']) ?></h1>
-</body>
-</html>
